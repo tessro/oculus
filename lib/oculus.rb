4e0da0806397e0dca7c8213284405c4ec50c3716
Paul Rosania <paul.rosania@gmail.com>
2012-03-28 23:10:42 -0700

    Basic configuration settings

--- lib/oculus.rb
+++ lib/oculus.rb
@@ -4,4 +4,18 @@ require "oculus/connection"
 require "oculus/query"
 
 module Oculus
+  extend self
+
+  attr_writer :cache_path
+
+  def cache_path
+    @cache_path ||= 'tmp/data'
+  end
+
+  attr_writer :data_store
+
+  def data_store
+    @data_store ||= Oculus::Storage::FileStore.new(Oculus.cache_path)
+  end
 end
+
