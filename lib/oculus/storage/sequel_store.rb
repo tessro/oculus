8452f6212b1c7a9af40e81e69d5566b63dd816a8
Jonathan Rudenberg <jonathan@titanous.com>
2012-06-19 19:50:17 -0400

    Refactor to disconnect after each storage query

--- lib/oculus/storage/sequel_store.rb
+++ lib/oculus/storage/sequel_store.rb
@@ -12,61 +12,76 @@ module Oculus
         create_table
       end
 
-      def db
-        Sequel.connect(@uri, :encoding => 'utf8')
+      def with_db
+        db = Sequel.connect(@uri, :encoding => 'utf8')
+        result = yield db
+        db.disconnect
+        result
       end
 
-      def table
-        db.from(@table_name)
+      def with_table
+        with_db { |db| yield db.from(@table_name) }
       end
 
       def all_queries
-        to_queries table.order(:id.desc)
+        with_table do |table|
+          to_queries table.order(:id.desc)
+        end
       end
 
       def starred_queries
-        to_queries table.where(:starred => true).order(:id.desc)
+        with_table do |table|
+          to_queries table.where(:starred => true).order(:id.desc)
+        end
       end
 
       def save_query(query)
         attrs = serialize(query)
-        if query.id
-          table.where(:id => query.id).update(attrs)
-        else
-          query.id = table.insert(attrs)
+        with_table do |table|
+          if query.id
+            table.where(:id => query.id).update(attrs)
+          else
+            query.id = table.insert(attrs)
+          end
         end
       end
 
       def load_query(id)
-        if query = table.where(:id => id).first
-          deserialize query
-        else
-          raise QueryNotFound, id
+        with_table do |table|
+          if query = table.where(:id => id).first
+            deserialize query
+          else
+            raise QueryNotFound, id
+          end
         end
       end
 
       def delete_query(id)
-        raise ArgumentError unless id.to_i > 0
-        raise QueryNotFound, id unless table.where(:id => id).delete == 1
+        with_table do |table|
+          raise ArgumentError unless id.to_i > 0
+          raise QueryNotFound, id unless table.where(:id => id).delete == 1
+        end
       end
 
       def create_table
-        db.create_table?(table_name) do
-          primary_key :id
-          Integer :thread_id
-          String :name
-          String :author
-          String :query
-          String :results
-          Time :started_at
-          Time :finished_at
-          TrueClass :starred
-          String :error
+        with_db do |db|
+          db.create_table?(table_name) do
+            primary_key :id
+            Integer :thread_id
+            String :name
+            String :author
+            String :query
+            String :results
+            Time :started_at
+            Time :finished_at
+            TrueClass :starred
+            String :error
+          end
         end
       end
 
       def drop_table
-        db.drop_table(table_name)
+        with_db { |db| db.drop_table(table_name) }
       end
 
       private
